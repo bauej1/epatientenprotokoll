52779019751bfaa1e57e1d1d391e79a072e53639
leameierx <meier.lea@icloud.com>
2018-11-19 10:08:32 +0100

    adapted master patient view and some functionalities

--- app/src/main/java/com/epatientenprotokoll/epatientenprotokoll/activities/MainActivity.java
+++ app/src/main/java/com/epatientenprotokoll/epatientenprotokoll/activities/MainActivity.java
@@ -1,11 +1,24 @@
 package com.epatientenprotokoll.epatientenprotokoll.activities;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 import com.epatientenprotokoll.epatientenprotokoll.R;
 import com.epatientenprotokoll.epatientenprotokoll.fragments.MasterDataFragment;
@@ -22,12 +35,14 @@ public class MainActivity extends AppCompatActivity {
     ToggleButton tbMasterData;
     ToggleButton tbStatus;
     ToggleButton tbMaterial;
-    Button bCurrentTime;
-    Button bDate;
-    Button bWeekday;
-    TextView tCurrentTime;
     TextView tDate;
     TextView tWeekday;
+    Button urgencyOne;
+    Button urgencyTwo;
+    ImageButton ibTime;
+    PopupWindow mpopup;
+    Context mConext;
+    LinearLayout vertLayout;
 
 
     @Override
@@ -38,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
         tbMasterData = findViewById(R.id.tbMasterData);
         tbStatus = findViewById(R.id.tbStatus);
         tbMaterial = findViewById(R.id.tbMaterial);
-        //bCurrentTime = findViewById(R.id.bCurrentTime);
-        //bDate = findViewById(R.id.bDate);
-        //bWeekday = findViewById(R.id.bWeekday);
-        //tCurrentTime = findViewById(R.id.tCurrentTime);
         tDate = findViewById(R.id.tDate);
         tWeekday = findViewById(R.id.tWeekday);
+        urgencyOne = findViewById(R.id.bUrgency1);
+        urgencyTwo = findViewById(R.id.bUrgency2);
+        ibTime = findViewById(R.id.ibTime);
+        vertLayout = findViewById(R.id.verticalLayout);
 
         tbMasterData.setOnClickListener(i -> {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -63,13 +78,6 @@ public class MainActivity extends AppCompatActivity {
             transaction.commit();
         });
 
-
-        Date currentTime = Calendar.getInstance().getTime();
-        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-        String currentTimeOutput = timeFormat.format(currentTime.getTime());
-        //bCurrentTime.setText(currentTimeOutput);
-        //tCurrentTime.setText(currentTimeOutput);
-
         Calendar cal = Calendar.getInstance();
         int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
         int month = (cal.get(Calendar.MONTH)) + 1;
@@ -77,13 +85,74 @@ public class MainActivity extends AppCompatActivity {
         String dayOfMonthString = String.valueOf(dayOfMonth);
         String monthString = String.valueOf(month);
         String yearString = String.valueOf(year);
-        //bDate.setText(dayOfMonthString + "." + monthString + "." + yearString);
         tDate.setText(dayOfMonthString + "." + monthString + "." + yearString);
 
         SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
         String dayOfWeek = dayFormat.format(cal.getTime());
-        //bWeekday.setText(dayOfWeek);
         tWeekday.setText(dayOfWeek);
+
+        urgencyOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, urgencyOne);
+                popup.getMenuInflater().inflate(R.menu.urgency_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        urgencyOne.setText(item.getTitle());
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+        urgencyTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, urgencyTwo);
+                popup.getMenuInflater().inflate(R.menu.urgency_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        urgencyTwo.setText(item.getTitle());
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+        ibTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View timeView = inflater.inflate(R.layout.time_view,null, false);
+
+                //mpopup = new PopupWindow(timeView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                //mpopup.showAsDropDown(vertLayout);
+
+                final TextView tAlarm = timeView.findViewById(R.id.tAlarm);
+                final EditText timeAlarm = timeView.findViewById(R.id.timeAlarm);
+                final Button bAlarm = timeView.findViewById(R.id.bAlarm);
+
+                new AlertDialog.Builder(MainActivity.this).setView(timeView).setTitle("Zeiten").show();
+
+
+                //AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                //builder.setTitle("Zeiten");
+
+                //TextView ziit = new TextView(MainActivity.this);
+                //ziit.setText("Alarm");
+                //builder.setView(ziit);
+
+                //AlertDialog dialog = builder.create();
+                //dialog.show();
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/epatientenprotokoll/epatientenprotokoll/fragments/MasterDataFragment.java
+++ app/src/main/java/com/epatientenprotokoll/epatientenprotokoll/fragments/MasterDataFragment.java
@@ -1,11 +1,29 @@
 package com.epatientenprotokoll.epatientenprotokoll.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ToggleButton;
+
 import com.epatientenprotokoll.epatientenprotokoll.R;
+
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * This class contains all controls for the master data and administrative data of the patient and the mission.
@@ -16,14 +34,137 @@ import com.epatientenprotokoll.epatientenprotokoll.R;
  */
 public class MasterDataFragment extends Fragment{
 
+    ToggleButton tGerman;
+    ToggleButton tFrench;
+    ToggleButton tMale;
+    ToggleButton tFemale;
+    MultiStateToggleButton mstbOperation;
+    MultiStateToggleButton mstbAccident;
+    List<String> operationArray;
+    List<String> accidentArray;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_master_data, container, false);
+
+
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        tGerman = getView().findViewById(R.id.tGerman);
+        tFrench = getView().findViewById(R.id.tFrench);
+        tMale = getView().findViewById(R.id.tMale);
+        tFemale = getView().findViewById(R.id.tFemale);
+        mstbOperation = getView().findViewById(R.id.mstb_multi_id);
+        mstbAccident = getView().findViewById(R.id.mstb_multi_id2);
+        operationArray = Arrays.asList(getResources().getStringArray(R.array.operation_array));
+        accidentArray = Arrays.asList(getResources().getStringArray(R.array.accident_array));
+
+
+        tGerman.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (tGerman.isChecked()) {
+                    tFrench.setChecked(false);
+                }
+            }
+        });
+
+        tFrench.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (tFrench.isChecked()) {
+                    tGerman.setChecked(false);
+                }
+            }
+        });
+
+        tMale.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (tMale.isChecked()) {
+                    tFemale.setChecked(false);
+                }
+            }
+        });
+
+        tFemale.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (tFemale.isChecked()) {
+                    tMale.setChecked(false);
+                }
+            }
+        });
+
+        mstbOperation.setOnValueChangedListener(new org.honorato.multistatetogglebutton.ToggleButton.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(int position) {
+                if (position == 1) {
+                    //final MultiStateToggleButton mstbAccident = new MultiStateToggleButton(getActivity());
+                    mstbAccident.setVisibility(View.VISIBLE);
+
+                    mstbAccident.setOnValueChangedListener(new org.honorato.multistatetogglebutton.ToggleButton.OnValueChangedListener() {
+                        @Override
+                        public void onValueChanged(int position) {
+//                            if (position == 0) {
+//                                operationArray.set(1, accidentArray.get(0));
+//                                mstbOperation.setElements(operationArray);
+//                            } else if (position == 1) {
+//                                operationArray.set(1, accidentArray.get(1));
+//                                mstbOperation.setElements(operationArray);
+//                            } else if (position == 2) {
+//                                operationArray.set(1, accidentArray.get(2));
+//                                mstbOperation.setElements(operationArray);
+//                            }
+
+                            operationArray.set(1, accidentArray.get(position));
+                            mstbOperation.setElements(operationArray);
+                            mstbAccident.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                }
+
+                if (position == 6){
+                    mstbAccident.setVisibility(View.INVISIBLE);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    final MultiStateToggleButton mstbAndere = new MultiStateToggleButton(getActivity());
+                    final EditText input = new EditText(getActivity());
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                    builder.setView(input);
+
+                    builder.setPositiveButton(R.string.speichern, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if (input.getText().toString().trim().length() > 0) {
+                                operationArray.set(6, input.getText().toString());
+                                mstbOperation.setElements(operationArray);
+                                Log.d(TAG, "operationArray" + operationArray);
+                            } else {
+                                //checkBox.toggle();
+                                return;
+                            }
+                        }
+                    })
+                            .setNegativeButton(R.string.abbrechen, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    return;
+                                }
+                            });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                    Button negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+                    negativeButton.setTextColor(Color.parseColor("#838182"));
+                }
+
+                Log.d(TAG, "Position: " + position);
+            }
+        });
 
     }
 }
