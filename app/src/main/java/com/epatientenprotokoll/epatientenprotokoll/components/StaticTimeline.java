e855724fd8dd48ceeb1c2406a7915425910a0e4f
leameierx <meier.lea@icloud.com>
2019-01-14 19:34:06 +0100

    adapted component class

--- app/src/main/java/com/epatientenprotokoll/epatientenprotokoll/components/StaticTimeline.java
+++ app/src/main/java/com/epatientenprotokoll/epatientenprotokoll/components/StaticTimeline.java
@@ -1,4 +1,4 @@
-package com.epatientenprotokoll.epatientenprotokoll.activities;
+package com.epatientenprotokoll.epatientenprotokoll.components;
 
 import android.content.Context;
 import android.graphics.Canvas;
